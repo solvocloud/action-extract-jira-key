3ff81efd420a2ef72c74ab6a69f9f562cac23436
Isaac Shabtay <isaac-solvo@users.noreply.github.com>
2022-12-11 16:25:59 +0200

    Added ignore pattern

--- resources/action.py
+++ resources/action.py
@@ -7,15 +7,21 @@ def main():
     parser.add_argument("--input", required=True)
     parser.add_argument("--group", type=int, required=True)
     parser.add_argument("--regex", required=True)
+    parser.add_argument("--ignore", required=False)
+
     args = parser.parse_args()
     input = args.input
     group = args.group
     regex = args.regex
+    ignore = args.ignore
 
+    result = re.match(ignore, input)
+    if result is not None:
+        print(f"::set-output name=ignore-validation::true")
+        return
     result = re.match(regex, input)
     if result is None:
         raise Exception(f"Failed extracting issue key from string: {input}")
-
     issue_key = result.group(group)
     print(f"::set-output name=issue-key::{issue_key}")
 
